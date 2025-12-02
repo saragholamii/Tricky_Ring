1523721b124cfb9901cd6a06106da0d729af1bee
saragholamii <ohartemis.2002@gmail.com>
2025-12-02 20:04:55 +0330

    player name and high score in start panel

--- Assets/Scripts/StartManager.cs
+++ Assets/Scripts/StartManager.cs
@@ -1,10 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StartManager : MonoBehaviour
 {
+    public TextMeshProUGUI playerName;
+    public TextMeshProUGUI highScore;
+
+    private void Start()
+    {
+        LoadPlayerName();
+        LeadHighScore();
+    }
+
+    private void LoadPlayerName()
+    {
+        playerName.text = PlayerPrefs.GetString("PlayerName", "Sara");
+    }
+
+    private void LeadHighScore()
+    {
+        highScore.text = "High Score: " +  PlayerPrefs.GetInt("HighScore", 0).ToString();
+    }
+    
     public void OnStartClicked()
     {
         SceneManager.LoadScene("Game");
